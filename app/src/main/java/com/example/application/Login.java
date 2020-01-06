7517c4940b7f5bf93708264586df8d455f799e28
hamuse <hamuseco@gmail.com>
2020-01-06 18:17:35 +0900

    first commit

---
+++ app/src/main/java/com/example/application/Login.java
@@ -0,0 +1,25 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        final Context _this = Login.this;
+
+        findViewById(R.id.btn_list).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(_this,MemberList.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/application/MemberUpdate.java
@@ -0,0 +1,25 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MemberUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_update);
+        final Context _this = MemberUpdate.this;
+
+        findViewById(R.id.btn_redetail).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(_this,MemberDetail.class));
+            }
+        });
+    }
+}
